df415d262fd7514578f5752d3fdd22d1c4c517dc
monisha96 <moni51196@gmail.com>
2018-10-06 18:35:15 -0700

    commit

---
+++ bb/src/components/header/header.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the HeaderComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'header',
+  templateUrl: 'header.html',
+  styleUrls: ['header.scss']
+})
+export class HeaderComponent {
+
+  text: string;
+
+  constructor() {
+    
+  }
+
+}
